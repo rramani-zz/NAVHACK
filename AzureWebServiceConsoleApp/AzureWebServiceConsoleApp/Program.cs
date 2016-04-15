74735c4a3477a9ce5ce9e18dedf135fd4818fdb0
raramani@microsoft.com <!Rfid123>
2016-04-15 09:46:03 -0700

    1) The Azure WebServiceConsoleApp is a copy paste from the Azure ML tutorial https://azure.microsoft.com/en-us/documentation/articles/machine-learning-r-csharp-retail-demand-forecasting/ 2) MachineLearningWebClient is the ASP.NET Web API app that front ends the requests from PBI and accesses the Forecast model from the Azure ML market place

---
+++ AzureWebServiceConsoleApp/AzureWebServiceConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureWebServiceConsoleApp
+{
+    class Program
+    {
+        public class Input
+        {
+            public string frequency;
+            public string horizon;
+            public string date;
+            public string value;
+        }
+
+        public AuthenticationHeaderValue CreateBasicHeader(string username, string password)
+        {
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(username + ":" + password);
+            return new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+
+        void Main()
+        {
+            var input = new Input() { frequency = TextBox1.Text, horizon = TextBox2.Text, date = TextBox3.Text, value = TextBox4.Text }; var json = JsonConvert.SerializeObject(input);
+            var acitionUri = "PutAPIURLHere,e.g.https://api.datamarket.azure.com/..../v1/Score";
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization = CreateBasicHeader("PutEmailAddressHere", "ChangeToAPIKey");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = httpClient.PostAsync(acitionUri, new StringContent(json));
+            var result = response.Result.Content;
+            var scoreResult = result.ReadAsStringAsync().Result;
+        }
+    }
+}
